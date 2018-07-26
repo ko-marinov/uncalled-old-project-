ab326e09a88d21fca88b1b17f6c70812080d4563
Sergey <seiro71@gmail.com>
2018-07-26 18:46:11 +0400

    Доработка героя. префаб. спрайты.скрипты

---
+++ Assets/Scripts/EnemyHp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHp : MonoBehaviour {
+
+    private float HP = 100;
+
+    public void Hit(float damage){
+        HP -= damage;
+        if (HP <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Fight2D.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fight2D : MonoBehaviour
+{
+
+    // функция возвращает ближайший объект из массива, относительно указанной позиции
+    static GameObject NearTarget(Vector3 position, Collider2D[] array)
+    {
+        Collider2D current = null;
+        float dist = Mathf.Infinity;
+
+        foreach (Collider2D coll in array)
+        {
+            float curDist = Vector3.Distance(position, coll.transform.position);
+
+            if (curDist < dist)
+            {
+                current = coll;
+                dist = curDist;
+            }
+        }
+
+        return (current != null) ? current.gameObject : null;
+    }
+
+    // point - точка контакта
+    // radius - радиус поражения
+    // layerMask - номер слоя, с которым будет взаимодействие
+    // damage - наносимый урон
+    // allTargets - должны-ли получить урон все цели, попавшие в зону поражения
+    public static void Action(Vector2 point, float radius, int layerMask, float damage, bool allTargets)
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(point, radius, 1 << layerMask);
+
+        if (!allTargets)
+        {
+            GameObject obj = NearTarget(point, colliders);
+            if (obj != null && obj.GetComponent<EnemyHp>())
+            {
+                //Destroy(obj);
+                obj.GetComponent<EnemyHp>().Hit(damage);
+            }
+            return;
+        }
+
+        foreach (Collider2D hit in colliders)
+        {
+            if (hit.GetComponent<EnemyHp>())
+            {
+                hit.GetComponent<EnemyHp>().Hit(damage);
+            }
+        }
+    }
+}
