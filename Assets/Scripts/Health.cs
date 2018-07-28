4d773a89658fecc0de387ec1fed96baff9d3efea
Konstantin Marinov <Marinov.KOHCTAHTIH@gmail.com>
2018-07-28 11:37:34 +0300

    EnemyHp renamed to Health. Usable for all damagable objects (Hero, Skeleton, etc.)

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class EnemyHp : MonoBehaviour {
+
+    public float maxHp = 100;
+
+    public delegate void Health0Args();
+    public event Health0Args OnHit;
+    public event Health0Args OnDeath;
+
+    public void Start()
+    {
+        currentHp = maxHp;
+    }
+
+    public void Hit(float damage){
+        if (IsDead())
+        {
+            return;
+        }
+
+        currentHp -= damage;
+        if (currentHp > 0)
+        {
+            OnHit();
+        }
+        else
+        {
+            currentHp = 0;
+            OnDeath();
+        }
+    }
+
+    public bool IsAlive()
+    {
+        return currentHp > 0;
+    }
+
+    public bool IsDead()
+    {
+        return !IsAlive();
+    }
+
+    // private
+
+    private float currentHp;
+}
