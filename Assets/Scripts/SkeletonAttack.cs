88b3b9e569c44feae42325366858f3659b173f5d
Konstantin Marinov <Marinov.KOHCTAHTIH@gmail.com>
2018-07-29 03:39:34 +0300

    Сделал атаку скелету: по AnimEvent-у наносит урон в круге перед скелетом объекту с Layer="Player"

---
+++ Assets/Scripts/SkeletonAttack.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonAttack : MonoBehaviour {
+
+    public float damage = 20;
+    public float radius = 0.5f;
+    public Vector3 localAttackPoint;
+
+    // private
+
+    private void AttackOnAnimEvent()
+    {
+        var layerMask = LayerMask.GetMask("Player");
+        var playerCollider = Physics2D.OverlapCircle(GetGlobalAttackPoint(), radius, layerMask);
+        if (playerCollider == null)
+        {
+            return;
+        }
+
+        var playerHealth = playerCollider.GetComponent<Health>();
+        if (playerHealth == null)
+        {
+            return;
+        }
+
+        playerHealth.Hit(damage);
+    }
+
+    private Vector3 GetGlobalAttackPoint()
+    {
+        var globalAttackPoint = localAttackPoint;
+        globalAttackPoint.x *= transform.localScale.x;
+        globalAttackPoint += transform.position;
+        return globalAttackPoint;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawSphere(GetGlobalAttackPoint(), radius);
+    }
+}
