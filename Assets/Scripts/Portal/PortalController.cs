d91913f9ec4799f2322c6529fdb9ac428823a776
Konstantin Marinov <Marinov.KOHCTAHTIH@gmail.com>
2018-08-04 20:01:51 +0300

    Добавил объект портал, анимации (idle, open, close) и скрипт-контроллер

---
+++ Assets/Scripts/Portal/PortalController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalController : MonoBehaviour {
+
+    public Animator animator;
+
+	public void Open()
+    {
+        animator.SetBool(PORTAL_OPEN_PARAMETER, true);
+    }
+
+    public void Close()
+    {
+        animator.SetBool(PORTAL_OPEN_PARAMETER, false);
+    }
+
+    // private
+
+    private const string PORTAL_OPEN_PARAMETER = "IsOpen";
+}
